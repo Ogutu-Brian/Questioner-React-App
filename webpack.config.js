2bfd71871457b7d1cff1da7c69627dff268bbbdd
Brian Ogutu <brianogutu@Brians-MacBook-Pro.local>
2019-04-07 14:08:51 +0300

    ft(signup):Signup To Questioner - Add external CSS style - Add bootstrap - Add signup form - Activate user during signup [Finishes #165164122]

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,46 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    },
+                ],
             }
         ]
     },
